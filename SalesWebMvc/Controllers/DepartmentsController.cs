0de88da427fc43016f5e08f00b7eca57c717d3bc
Renata Cruz <rena-cruz@hotmail.com>
2022-01-18 15:11:05 -0300

    Other entities and second migration

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
-using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
