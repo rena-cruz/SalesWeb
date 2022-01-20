59c5f4c80517c2f1eec25e53ed759611886e0469
Renata Cruz <rena-cruz@hotmail.com>
2022-01-20 18:35:54 -0300

    Change in dates SeedingService

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
 {
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,10 +1,8 @@
-﻿using SalesWebMvc.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services.Exceptions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
